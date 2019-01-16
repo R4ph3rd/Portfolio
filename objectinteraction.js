e430d433f2f4d522635a6a75994b21fd5600fa95
Raphaël Perraud <43202876+R4ph3rd@users.noreply.github.com>
2019-01-17 00:35:11 +0100

    Add files via upload

--- objectinteraction.js
+++ objectinteraction.js
@@ -70,6 +70,22 @@ function setup() {
     }
 }
 
+function handleOrientation(event) {
+  var x = event.beta;  // In degree in the range [-180,180]
+  var y = event.gamma; // In degree in the range [-90,90]
+
+  // Because we don't want to have the device upside down
+  // We constrain the x value to the range [-90,90]
+  if (x >  90) { x =  90};
+  if (x < -90) { x = -90};
+
+  // To make computation easier we shift the range of 
+  // x and y to [0,180]
+  x += 90;
+  y += 90;
+    console.log(x)
+}
+
 function draw() {
     console.log(massPoint)
     noStroke();
